6ad678e9350eb83c962d0606fad4b8b479ed9871
Dan <dmm4613@gmail.com>
2020-09-21 22:00:04 -0400

    just messing around with some basic web dev

---
+++ main.js
@@ -0,0 +1,16 @@
+let potionAdjective = ["Dirty", "Smelly", "Moldy", "Lumpy", "Shiny", "Wrinkly", "Slow", "Fast", "Old", "Youthful", "Hairy", "Scary", "Horny", "Spicy", "Salty", "Sweet", "Buttery", "Crunchy"]
+let potionFirstNoun = ["Armadillo", "Bat", "Newt", "Dragon", "Rabbit", "Unicorn", "Skeleton", "Turtle", "Centaur", "Goblin", "Orc", "Weasel", "Djinni", "Karen", "Minotaur", "Rock", "Cricket", "Sponge", "Shark", "Goat", "Tucan"]
+let potionSecondNoun = ["Spleen", "Skin", "Scale", "Tail", "Eye", "Rib", "Heart", "Brain", "Lung", "Toe", "Penis", "Anus", "Leg", "Intestine", "Ear", "Lip", "Tongue", "Nose", "Tendon", "Finger", "Liver", "Bladder", "Stomach", "Oil"]
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+
+function generatePotion() {
+    document.getElementById('potion-description').innerHTML =
+        `Potion of 
+    ${potionAdjective[getRandomInt(potionAdjective.length)]}
+    ${potionFirstNoun[getRandomInt(potionFirstNoun.length)]}
+    ${potionSecondNoun[getRandomInt(potionSecondNoun.length)]}`
+}
