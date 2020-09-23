7127ee9e9d8ed1ff136432f3a3890f76b1c84953
Dan <dmm4613@gmail.com>
2020-09-23 16:37:33 -0400

    fixed the speech bubble and started randomizing text

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let potionAdjective = ["Dirty", "Smelly", "Moldy", "Lumpy", "Shiny", "Wrinkly", "Slow", "Fast", "Old", "Youthful", "Hairy", "Scary", "Horny", "Spicy", "Salty", "Sweet", "Buttery", "Crunchy"]
 let potionFirstNoun = ["Armadillo", "Bat", "Newt", "Dragon", "Rabbit", "Unicorn", "Skeleton", "Turtle", "Centaur", "Goblin", "Orc", "Weasel", "Djinni", "Minotaur", "Rock", "Cricket", "Sponge", "Shark", "Goat", "Tucan"]
 let potionSecondNoun = ["Spleen", "Skin", "Scale", "Tail", "Eye", "Rib", "Heart", "Brain", "Lung", "Toe", "Penis", "Anus", "Leg", "Intestine", "Ear", "Lip", "Tongue", "Nose", "Tendon", "Finger", "Liver", "Bladder", "Stomach", "Oil"]
-
+let naklakPhrase = ["'S good. You like! Make real strong.", "Very sweet. You like!"]
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max))
@@ -11,9 +11,10 @@ function generatePotion() {
     let adjective = potionAdjective[getRandomInt(potionAdjective.length)]
     let firstNoun = potionFirstNoun[getRandomInt(potionFirstNoun.length)]
     let secondNoun = potionSecondNoun[getRandomInt(potionSecondNoun.length)]
-    document.getElementById('naklak').classList.remove('silent');
+    let naklak = naklakPhrase[getRandomInt(naklakPhrase.length)]
+    document.getElementById('speech-bubble').classList.remove('silent');
     document.getElementById('potion-description').innerHTML =
         `Potion of ${adjective} ${firstNoun} ${secondNoun}`;
-    document.getElementById('naklak').innerText = `Oooh. ${secondNoun} from ${firstNoun}. 'S good. You like. Make real strong.`;
+    document.getElementById('naklak').innerHTML = `Oooh. ${secondNoun} from ${firstNoun}. ${naklak}`;
     document.getElementById('title').classList.add('title-change');
 }
